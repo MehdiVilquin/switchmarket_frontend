4790358515d7da199142f10d308569eaa3a4ec57
Anthony Gibilaro <dev@gibilab.com>
2025-04-15 15:34:24 +0200

    fix progress UI components

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -11,6 +11,7 @@ function Progress({
   ...props
 }) {
   return (
+    (<ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
         "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
@@ -21,6 +22,7 @@ function Progress({
         data-slot="progress-indicator"
         className="bg-primary h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
+    </ProgressPrimitive.Root>)
   );
 }
 
