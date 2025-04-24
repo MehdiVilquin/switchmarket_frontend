730f4c17f03f33ec306aba0c30456dcb7a9d82b0
MehdiVilquin <vilquin.mehdi@gmail.com>
2025-04-24 17:31:33 +0200

    deploiement_frond

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
-// Global configuration file for frontend
-// Change BASE_APIURL when deploying.
+// config.js
 
-// export const BASE_APIURL = "http://localhost:3000";
+// Global configuration file for frontend
+// BASE_APIURL will be set from environment variable at build time.
+
 export const BASE_APIURL =
-  "https://switchmarket-backend-ca0s5z6av-vilquinmehdi-gmailcoms-projects.vercel.app";
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://switchmarket-backend-ca0s5z6av-vilquinmehdi-gmailcoms-projects.vercel.app" ||
+  "http://localhost:3000";
 
 // If you want to override via environment variable uncomment below and comment above line.
 // export const BASE_APIURL = process.env.NEXT_PUBLIC_BASE_APIURL || "http://localhost:3000";
